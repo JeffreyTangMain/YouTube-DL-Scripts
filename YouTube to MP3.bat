cf1ea3441a31fc09428fff395771fc5a16cf4a4a
ImmortalZypther <97859002+JeffreyTangMain@users.noreply.github.com>
2022-06-05 09:20:27 -0400

    2.0 - Reworked All Scripts
    Forgot the YouTube to MP3 script in the previous commit. Oops!

---
+++ YouTube to MP3.bat
@@ -0,0 +1,23 @@
+echo off
+
+set location=%~dp0
+set downloadCommand="%location%\%%(title)s.%%(ext)s"
+
+IF EXIST yt-dlp.exe (
+    set downloadCommand="%location%\Downloaded Video\%%(title)s.%%(ext)s"
+) ELSE (
+    cd..
+)
+
+for /f "usebackq delims=" %%A in ("0_Variables.txt") do %%A
+set /p url=URL: 
+
+IF NOT %ERRORLEVEL%==0 GOTO :justUpdate
+
+:download
+%downloadMP3% %downloadCommand% %url%
+exit
+
+:justUpdate
+%update%
+GOTO download
